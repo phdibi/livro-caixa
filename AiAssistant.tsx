6302e139847753c93b4912343aa91c2b79073a3f
phdibi <phdibi@gmail.com>
2025-11-20 19:16:22 -0300

    Update AiAssistant.tsx

--- AiAssistant.tsx
+++ AiAssistant.tsx
@@ -1,5 +1,4 @@
-
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { MicrophoneIcon, SendIcon, SparklesIcon, StopIcon } from './Icons';
 import { Transaction, Account } from '../types';
 import { parseTransactionFromText, transcribeAudio } from '../services/geminiService';
