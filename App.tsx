1e57a1351581afa244fea874e1a16f1e664cd2bd
phdibi <phdibi@gmail.com>
2025-12-09 17:23:10 -0300

    up bug

--- App.tsx
+++ App.tsx
@@ -26,6 +26,7 @@ const AppContent: React.FC = () => {
     dataLoading,
     isSyncing,
     filteredTransactions,
+    transactions,
     accounts,
     recurringTransactions,
     setRecurringTransactions,
